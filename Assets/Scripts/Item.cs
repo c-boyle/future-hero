0237e0e7ab9d12f203672be111c96b81f787ede9
c-boyle <caalleebb1@gmail.com>
2022-09-30 01:00:54 -0400

    Add item name to item class to use for equality comparison

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -3,5 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Item : MonoBehaviour {
-   
+  // Name acts as a key
+  [SerializeField] private string itemName;
+
+  public override bool Equals(object other) {
+    return base.Equals(other);
+  }
+
+  public override int GetHashCode() {
+    return itemName.GetHashCode();
+  }
+
+  public static bool operator ==(Item a, Item b) {
+    return a.itemName == b.itemName;
+  }
+  public static bool operator !=(Item a, Item b) {
+    return a.itemName != b.itemName;
+  }
+
+
 }
