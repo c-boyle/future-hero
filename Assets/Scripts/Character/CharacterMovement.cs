0eca08a72c4d747e3a535a07482953a72636daaf
c-boyle <caalleebb1@gmail.com>
2022-10-04 02:02:28 -0400

    Fiddled around trying to get path movement to use character movement script

--- Assets/Scripts/Character/CharacterMovement.cs
+++ Assets/Scripts/Character/CharacterMovement.cs
@@ -2,40 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterMovement : MonoBehaviour
-{
-    [SerializeField] private Rigidbody _rigidbody;
-    [SerializeField] private Transform _bodyTransform, _cameraTransform; 
-    private float _movementSpeed = 10f;
-    private float _rotationSpeed = 3f;
+public class CharacterMovement : MonoBehaviour {
+  [SerializeField] private Rigidbody _rigidbody;
+  [SerializeField] private Transform _bodyTransform, _cameraTransform;
+  [SerializeField] private float _movementSpeed = 10f;
+  private float _rotationSpeed = 3f;
 
-    // Function that moves character
-    public void Move(Vector2 movement)
-    {   
-        // Temporary Velocity variable that we'll set player velocity to later (for now until comfortable with impl.)
-        Vector3 tmpVelocity;
+  // Function that moves character
+  public void Move(Vector2 movement) {
+    // Temporary Velocity variable that we'll set player velocity to later (for now until comfortable with impl.)
+    Vector3 tmpVelocity;
 
-        // Debug.Log(movement);
+    // Debug.Log(movement);
 
-        tmpVelocity = _bodyTransform.forward * movement.x;
-        tmpVelocity += _bodyTransform.right * -movement.y;
+    tmpVelocity = _bodyTransform.right * movement.x;
+    tmpVelocity += _bodyTransform.forward * movement.y;
 
-        _rigidbody.velocity = tmpVelocity * _movementSpeed;
+    _rigidbody.velocity = tmpVelocity * _movementSpeed;
+  }
+
+
+  // Function that changes where the character is looking
+  public void Look(Vector2 rotation) {
+    //Debug.Log(rotation);
+
+    Vector3 playerRotation = _bodyTransform.rotation.eulerAngles;
+
+    playerRotation.y += rotation.x * _rotationSpeed;
+
+    _bodyTransform.rotation = Quaternion.Euler(playerRotation);
+    if (_cameraTransform != null) {
+      Vector3 cameraRotation = _cameraTransform.rotation.eulerAngles;
+      cameraRotation.x -= rotation.y * _rotationSpeed;
+      _cameraTransform.rotation = Quaternion.Euler(cameraRotation);
     }
+  }
 
-
-    // Function that changes where the character is looking
-    public void Look(Vector2 rotation)
-    {
-        //Debug.Log(rotation);
-    
-        Vector3 cameraRotation = _cameraTransform.rotation.eulerAngles;
-        Vector3 playerRotation = _bodyTransform.rotation.eulerAngles;
-    
-        cameraRotation.x -= rotation.y * _rotationSpeed;
-        playerRotation.y += rotation.x * _rotationSpeed;
-
-        _cameraTransform.rotation = Quaternion.Euler(cameraRotation);
-        _bodyTransform.rotation = Quaternion.Euler(playerRotation);
-    }
+  public void LookInDirection(Vector3 direction) {
+    var lookRotation = Quaternion.LookRotation(direction.normalized, Vector3.up);
+    var temp = Quaternion.Slerp(_bodyTransform.rotation, lookRotation, Time.deltaTime * _rotationSpeed);
+    _bodyTransform.rotation = Quaternion.Euler(0f, temp.eulerAngles.y, 0f);
+  }
 }
--- Assets/Scripts/Character/PathMovement.cs
+++ Assets/Scripts/Character/PathMovement.cs
@@ -2,79 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PathMovement : MonoBehaviour
-{
-    private enum PathChoice
-    {
-        pathA,
-        pathB
+public class PathMovement : MonoBehaviour {
+
+  [SerializeField] private List<Path> paths;
+  [SerializeField] private CharacterMovement movement;
+  [SerializeField] private int pointOfNoReturn = 2;
+  [SerializeField] private int currentPathIndex = 0;
+  private int currentPathPoint = 0;
+
+  // Update is called once per frame
+  void Update() {
+    if (currentPathPoint >= paths[currentPathIndex].PathPoints.Count) {
+      movement.Move(Vector2.zero);
+      return;
     }
 
-
-    [SerializeField] private List<Transform> pathA;
-    [SerializeField] private List<Transform> pathB;
-    [SerializeField] private Rigidbody rb;
-    [SerializeField] private float velocity;
-    [SerializeField] private int pointOfNoReturn = 2;
-    private int currentPathPoint = 0;
-    private PathChoice pathChoice = PathChoice.pathA;
-
-    private float trainStartDelay = 6f;
-
-    private List<Transform> Path { 
-        get { 
-          switch (pathChoice)
-            {
-                case PathChoice.pathA:
-                    return pathA;
-                case PathChoice.pathB:
-                    return pathB;
-                default:
-                    return pathA;
-            }
-        } 
+    var targetPosition = paths[currentPathIndex].PathPoints[currentPathPoint].position;
+    Vector3 direction = targetPosition - movement.transform.position;
+    direction.y = 0f;
+    
+    if (direction.magnitude <= 4f) {
+      currentPathPoint++;
+      return;
     }
+    Debug.Log(direction);
+    Debug.DrawRay(movement.transform.position, direction, Color.cyan);
+    movement.LookInDirection(direction);
+    /*
+    direction = new(direction.z, -direction.x, 0f);
+    Vector2 movementInput = direction.normalized;
+    Debug.Log("movementInput: " + movementInput);
+    */
+    movement.Move(new Vector2(0f, 1f));
+  }
 
-
-
-    // Update is called once per frame
-    void Update()
-    {
-        trainStartDelay -= Time.deltaTime;
-        if (trainStartDelay > 0)
-        {
-            return;
-        }
-        if (currentPathPoint >= Path.Count)
-        {
-            rb.velocity = Vector3.zero;
-            return;
-        }
-
-        Vector3 direction = Path[currentPathPoint].position - rb.position;
-        direction = new(direction.x, 0f, direction.z);
-        if (direction.magnitude <= 4f)
-        {
-            currentPathPoint++;
-            return;
-        }
-
-        rb.velocity = direction.normalized * velocity;
-        transform.LookAt(transform.position + direction, Vector3.up);
+  public void SetPath(int pathIndex) {
+    if (currentPathPoint >= pointOfNoReturn) {
+      return;
     }
-
-    public void SwitchPath()
-    {
-        if (currentPathPoint >= pointOfNoReturn)
-        {
-            return;
-        }
-        if (pathChoice == PathChoice.pathA)
-        {
-            pathChoice = PathChoice.pathB;
-        } else
-        {
-            pathChoice = PathChoice.pathB;
-        }
+    if (pathIndex >= 0 && pathIndex < paths.Count) {
+      currentPathIndex = pathIndex;
+    } else {
+      Debug.LogError("Attempt to set the current path of " + this.gameObject.name + " to an path index that is out of bounds.");
     }
+  }
+
+  [System.Serializable]
+  private class Path {
+    [field: SerializeField] public List<Transform> PathPoints { get; set; }
+  }
 }
