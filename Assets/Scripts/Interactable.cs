44842c49cfccf2af190e90d6dc44fc23d5635dce
c-boyle <caalleebb1@gmail.com>
2022-10-03 21:13:33 -0400

    Change interactable script to disallow interacting an item you're holding, and ensure only the closest interactable is used

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -6,27 +6,38 @@ using UnityEngine.Events;
 
 public class Interactable : MonoBehaviour {
   [SerializeField] private UnityEvent interactionAction;
-  [Tooltip("Set to none if no item is required to interact.")] [SerializeField] private Item requireItem = null;
+  [Tooltip("Set to none if no item is required to interact.")][SerializeField] private Item requireItem = null;
   [SerializeField] private bool destroyRequiredItemOnInteraction = false;
   [SerializeField] private Item giveItem = null;
   [SerializeField] private bool disableAfterFirstUse = false;
 
   private bool firstUse = true;
 
+  private readonly static HashSet<Interactable> enabledInteractables = new();
+
+  private const float maxInteractionRange = 15f;
+
   private void OnEnable() {
-    BaseInput.Interaction += OnInteract;
+    enabledInteractables.Add(this);
+    if (enabledInteractables.Count == 1) {
+      BaseInput.Interaction += UseClosestInteractable;
+    }
   }
 
   private void OnDisable() {
-    BaseInput.Interaction -= OnInteract;
+    enabledInteractables.Remove(this);
+    if (enabledInteractables.Count == 0) {
+      BaseInput.Interaction -= UseClosestInteractable;
+    }
   }
 
   private void OnInteract(object sender, InteractionEventArgs e) {
+
     if (!gameObject.activeSelf || (disableAfterFirstUse && !firstUse)) {
       return;
     }
     bool meetsItemRequirement = requireItem == null || (e.ItemHolder != null && requireItem == e.ItemHolder.HeldItem);
-    if (Vector3.Distance(e.InteractorPosition, transform.position) <= 15f && meetsItemRequirement) {
+    if (meetsItemRequirement) {
       if (giveItem != null && e.ItemHolder != null) {
         e.ItemHolder.GrabItem(giveItem);
       }
@@ -38,6 +49,24 @@ public class Interactable : MonoBehaviour {
       interactionAction?.Invoke();
       firstUse = !firstUse;
       Debug.Log(name + " interacted");
+    }
+  }
+
+  private static void UseClosestInteractable(object sender, InteractionEventArgs e) {
+    float closestInteractableDist = Mathf.Infinity;
+    Interactable closestInteractable = null;
+    foreach (var interactable in enabledInteractables) {
+      bool interactorIsHoldingThisInteractable = e.ItemHolder != null && e.ItemHolder.HeldItem != null && e.ItemHolder.HeldItem.Interactable == interactable;
+      if (!interactorIsHoldingThisInteractable) { // Skip an interactable if it's being held by the interactor
+        float dist = Vector3.Distance(e.InteractorPosition, interactable.transform.position);
+        if (dist < closestInteractableDist) {
+          closestInteractableDist = dist;
+          closestInteractable = interactable;
+        }
+      }
+    }
+    if (closestInteractableDist <= maxInteractionRange && closestInteractable != null) {
+      closestInteractable.OnInteract(sender, e);
     }
   }
 
