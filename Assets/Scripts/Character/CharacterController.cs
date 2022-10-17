a9ed740e4d8ff6ff43822a6b6bb898f1310aaa66
Elson Liang <elsonliang@gmail.com>
2022-10-17 01:15:48 -0700

    Implement air movement

--- Assets/Scripts/Character/CharacterController.cs
+++ Assets/Scripts/Character/CharacterController.cs
@@ -7,27 +7,32 @@ public class CharacterController : MonoBehaviour {
     [SerializeField] [MustBeAssigned] private Rigidbody _rigidbody;
     [SerializeField] [MustBeAssigned] private Transform _bodyTransform;
     [SerializeField] [MustBeAssigned] private Transform _cameraTransform;
+    [SerializeField] [MustBeAssigned] private Transform _groundCheckTransform;
+    [SerializeField] [MustBeAssigned] private LayerMask _groundLayer;
     [SerializeField] [MustBeAssigned] private Animation _watchArmAnimation;
+
 
     [SerializeField] [ReadOnly] private float pitchDegree = 0f;
     [SerializeField] [ReadOnly] private float yawDegree = 0f;
 
     [PositiveValueOnly] public float SENSITIVITY = 1f;  // Mouse sensitivity
-    private const float GROUND_ACCELERATION = 20f; // How fast the player gains speed on ground
-    private const float AIR_ACCELERATION = 10f; // How fast the player gains speed while in the air
     private const float MAX_PITCH_DEGREE = 60; // How high or low the player can raise their head
-    private const float MAX_VELOCITY = 15f; // Maximum speed the player can go
-    private const float FRICTION = 5f; // How quickly the player slows down
-    private const float JUMP_INTENSITY = 12f; // How high the player jumps
+    private const float GROUND_MAX_VELOCITY = 15f; // Maximum speed the player can go on ground
+    private const float AIR_MAX_VELOCITY = 12f; // Maximum speed the player can go while in the air
+    private const float GROUND_ACCELERATION = 1f; // How fast the player gains speed on ground
+    private const float AIR_ACCELERATION = 0.02f; // How fast the player gains speed while in the air
+    private const float GROUND_FRICTION = 5f; // How quickly the player slows down while on the ground
+    private const float AIR_FRICTION = 1f; // How quickly the player slows down while in the air
+    private const float JUMP_INTENSITY = 10f; // How high the player jumps
 
     // Function that gets called each time move inputs are used
     public void Move(Vector2 movement) {
         Vector3 moveVector = _bodyTransform.right * movement.x + _bodyTransform.forward * movement.y;
-        Vector3 deltaVelocity = moveVector * GROUND_ACCELERATION;
+        Vector3 deltaVelocity = moveVector * (IsGrounded() ? GROUND_ACCELERATION : AIR_ACCELERATION);
 
         Vector3 newVelocityXZ = deltaVelocity + _rigidbody.velocity;
         newVelocityXZ.y = 0;
-        newVelocityXZ = Vector3.ClampMagnitude(newVelocityXZ, MAX_VELOCITY);
+        newVelocityXZ = Vector3.ClampMagnitude(newVelocityXZ, (IsGrounded() ? GROUND_MAX_VELOCITY : AIR_MAX_VELOCITY));
 
         _rigidbody.velocity = newVelocityXZ + new Vector3(0, _rigidbody.velocity.y, 0);
     }
@@ -36,14 +41,12 @@ public class CharacterController : MonoBehaviour {
     public void Look(Vector2 viewDirection) {
         Vector3 cameraRotation = ConvertAngle(_cameraTransform.localRotation.eulerAngles);
         Vector3 playerRotation = ConvertAngle(_bodyTransform.rotation.eulerAngles);
-        setView(cameraRotation.x - viewDirection.y * SENSITIVITY, playerRotation.y + viewDirection.x * SENSITIVITY);
+        SetView(cameraRotation.x - viewDirection.y * SENSITIVITY, playerRotation.y + viewDirection.x * SENSITIVITY);
     }
 
     // Function that lets the Future Hero Jump
     public void Jump() {
-        if (_rigidbody.velocity.y < 0.1) {
-            _rigidbody.AddForce(_bodyTransform.up * JUMP_INTENSITY, ForceMode.Impulse);
-        }
+        if (IsGrounded()) _rigidbody.AddForce(_bodyTransform.up * JUMP_INTENSITY, ForceMode.Impulse);
     }
 
     // Function that triggers the animation to look at the watch
@@ -64,7 +67,7 @@ public class CharacterController : MonoBehaviour {
      * But I'm too lazy to assign friction to every material and drag shouldn't be used,
      * so here is this function that gets called each timestep and does things automatically.
      */
-    private Vector3 AddFriction(Vector3 currentVel, float friction, bool isYAffected=false) {
+    private Vector3 AddFriction(Vector3 currentVel, float friction, bool isYAffected = false) {
         float speed = currentVel.magnitude;
         Vector3 newVel = Vector3.zero;
 
@@ -75,13 +78,13 @@ public class CharacterController : MonoBehaviour {
 
             newVel = Vector3.Scale(currentVel, new Vector3(scale, isYAffected ? scale : 1, scale));
         }
-        
+
         return newVel;
     }
 
     // Because I'm too lazy to set friction on every material manually
     void FixedUpdate() {
-        _rigidbody.velocity = AddFriction(_rigidbody.velocity, FRICTION);
+        _rigidbody.velocity = AddFriction(_rigidbody.velocity, IsGrounded() ? GROUND_FRICTION : AIR_FRICTION);
     }
 
     // Helper function to convert a wrapped angle to a non wrapped angle (etc. 270 -> -90)
@@ -94,7 +97,7 @@ public class CharacterController : MonoBehaviour {
     }
 
     // Function to set the camera perspective directly
-    public void setView(float pitch, float yaw) {
+    public void SetView(float pitch, float yaw) {
         Vector3 cameraRotation = ConvertAngle(_cameraTransform.localRotation.eulerAngles);
         pitchDegree = Mathf.Clamp(pitch, -MAX_PITCH_DEGREE, MAX_PITCH_DEGREE);
         _cameraTransform.localRotation = Quaternion.Euler(pitchDegree, cameraRotation.y, cameraRotation.z);
@@ -102,5 +105,9 @@ public class CharacterController : MonoBehaviour {
         Vector3 playerRotation = ConvertAngle(_bodyTransform.rotation.eulerAngles);
         yawDegree = yaw;
         _bodyTransform.rotation = Quaternion.Euler(playerRotation.x, yawDegree, playerRotation.z);
+    }
+
+    public bool IsGrounded() {
+        return Physics.CheckSphere(_groundCheckTransform.position, 1f, _groundLayer);
     }
 }
