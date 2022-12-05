b4da441a7ab311946f30561d1d7e0c453f3af499
c-boyle <caalleebb1@gmail.com>
2022-12-05 03:18:06 -0500

    Add picking up cup sound and glass breaking sound

--- Assets/Scripts/CupFall.cs
+++ Assets/Scripts/CupFall.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CupFall : Anim
 {
     [SerializeField] GameObject puddle;
+    [SerializeField] private AudioSource smashSound;
     // Start is called before the first frame update
     protected override IEnumerator TriggerAfterAnimation(Animation animate) {
         while(animate.isPlaying) yield return null;
@@ -12,6 +13,7 @@ public class CupFall : Anim
         yield return new WaitForSeconds(0.2f);
 
         if (puddle != null) puddle.SetActive(true);
+        smashSound.Play();
         gameObject.SetActive(false);
     }
 }
