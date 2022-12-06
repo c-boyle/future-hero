0133a3009004e0d128425878c3c4db6a04acbd1a
c-boyle <caalleebb1@gmail.com>
2022-12-06 15:41:31 -0500

    Complete newspaper view

--- Assets/Scripts/GameEndData.cs
+++ Assets/Scripts/GameEndData.cs
@@ -4,20 +4,25 @@ using UnityEngine;
 
 [CreateAssetMenu(fileName = nameof(GameEndData), menuName = nameof(GameEndData))]
 public class GameEndData : ScriptableObject {
-  public string Headline { get { endingsSeen.Add(CurrentEndingID); return endingIDToEndingData[CurrentEndingID].Headline; } }
-  public string Summary { get { endingsSeen.Add(CurrentEndingID); return endingIDToEndingData[CurrentEndingID].Summary; } }
-  public Sprite Picture { get { endingsSeen.Add(CurrentEndingID); return endingIDToEndingData[CurrentEndingID].Picture; } }
+  public string Headline { get { endingsSeen.Add(CurrentEndingID); return EndingIDToEndingData[CurrentEndingID].Headline; } }
+  public string Summary { get { endingsSeen.Add(CurrentEndingID); return EndingIDToEndingData[CurrentEndingID].Summary; } }
+  public Sprite Picture { get { endingsSeen.Add(CurrentEndingID); return EndingIDToEndingData[CurrentEndingID].Picture; } }
   public int TotalEndings { get => endings.Count; }
   public int EndingsSeen { get => endingsSeen.Count; }
   [field: SerializeField] public string CurrentEndingID { get; set; } = null;
   [SerializeField] private readonly HashSet<string> endingsSeen = new();
   [SerializeField] private List<SummaryData> endings = new();
 
-  private readonly Dictionary<string, SummaryData> endingIDToEndingData = new();
-
-  private void Awake() {
-    foreach (var ending in endings) {
-      endingIDToEndingData[ending.EndingID] = ending;
+  private Dictionary<string, SummaryData> _endingIDToEndingData = null;
+  private Dictionary<string, SummaryData> EndingIDToEndingData {
+    get {
+      if (_endingIDToEndingData == null) {
+        _endingIDToEndingData = new();
+        foreach (var ending in endings) {
+          _endingIDToEndingData[ending.EndingID] = ending;
+        }
+      }
+      return _endingIDToEndingData;
     }
   }
 
@@ -25,7 +30,7 @@ public class GameEndData : ScriptableObject {
   private class SummaryData {
     [field: SerializeField] public string EndingID { get; set; }
     [field: SerializeField] public string Headline { get; set; }
-    [field: SerializeField] public string Summary { get; set; }
+    [field: SerializeField][field: TextArea] public string Summary { get; set; }
     [field: SerializeField] public Sprite Picture { get; set; }
   }
 }
--- Assets/Scripts/UI/NewspaperView.cs
+++ Assets/Scripts/UI/NewspaperView.cs
@@ -9,15 +9,21 @@ public class NewspaperView : MonoBehaviour {
   [SerializeField] private TMP_Text summaryText;
   [SerializeField] private Image picture;
   [SerializeField] private GameEndData gameEndData;
+  [SerializeField] private Animator animator;
+
+  private float inputBlockTime;
 
   private void Start() {
-    headlineText.text = gameEndData.Headline;
+    headlineText.text = gameEndData.Headline.ToUpper();
     summaryText.text = gameEndData.Summary;
     picture.sprite = gameEndData.Picture;
-    // Begin display animation
+    inputBlockTime = animator.GetCurrentAnimatorClipInfo(0)[0].clip.length;
   }
 
   private void Update() {
     // Once the display animation is over, check for any input, then show the game end menu
+    if (inputBlockTime <= 0 && Input.anyKeyDown) {
+      
+    }
   }
 }
